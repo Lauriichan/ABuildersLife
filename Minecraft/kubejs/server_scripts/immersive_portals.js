9489d7add3d4b7fd1f59901959762e4e7f63df82
Lauriichan <lauriichan@playuniverse.org>
2024-03-15 21:18:45 +0100

    Add custom immersive portals

---
+++ Minecraft/kubejs/server_scripts/immersive_portals.js
@@ -0,0 +1,12 @@
+ServerEvents.recipes(event => {
+    event.shaped('1x abuilderslife:portal_core', [
+        'RDR',
+        'EGE',
+        'RDR'
+    ], {
+        D: 'minecraft:diamond',
+        E: 'minecraft:ender_pearl',
+        R: 'minecraft:redstone',
+        G: 'minecraft:ghast_tear'
+    });
+});
---
+++ Minecraft/kubejs/startup_scripts/immersive_portals.js
@@ -0,0 +1,6 @@
+StartupEvents.registry('item', event => {
+    event.create("abuilderslife:portal_core")
+        .texture("abuilderslife:portal_core")
+        .maxStackSize(16)
+        .displayName("Portal Core");
+});
