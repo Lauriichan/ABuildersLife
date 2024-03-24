5e926289cde2cc13a86bcdb07e3fb3e9661aff5a
Lauriichan <lauriichan@playuniverse.org>
2024-03-24 16:36:26 +0100

    Fix consistency.js

--- Minecraft/kubejs/server_scripts/recipes/consistency.js
+++ Minecraft/kubejs/server_scripts/recipes/consistency.js
@@ -1,6 +1,7 @@
 ServerEvents.recipes(event => {
-    event.replaceInput({ input: 'consistency_plus:copper_nugget' }, 'consistency_plus:coppy_nugget', 'create:copper_nugget');
     event.replaceInput({ input: 'indrev:copper_nugget' }, 'indrev:coppy_nugget', 'create:copper_nugget');
-    event.replaceOutput({ output: 'consistency_plus:copper_nugget' }, 'consistency_plus:coppy_nugget', 'create:copper_nugget');
     event.replaceOutput({ output: 'indrev:copper_nugget' }, 'indrev:coppy_nugget', 'create:copper_nugget');
+    event.replaceOutput({ output: 'consistency_plus:copper_nugget', input: 'minecraft:spyglass' }, 'consistency_plus:copper_nugget', 'create:copper_nugget');
+    event.remove({ input: 'consistency_plus:copper_nugget' });
+    event.remove({ output: 'consistency_plus:copper_nugget' });
 });
